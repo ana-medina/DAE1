0531d4fc50d5c62997f1810c678c332988b55929
Alexandra Martinez <nahomibj@gmail.com>
2017-02-14 13:41:50 -0600

    Added tests

--- spring-service/src/test/java/com/martinez/test/service/BookServiceTest.java
+++ spring-service/src/test/java/com/martinez/test/service/BookServiceTest.java
@@ -1,4 +1,24 @@
 package com.martinez.test.service;
 
-public class BookServiceTest {
+import com.martinez.entities.Book;
+import com.martinez.test.config.ServiceContextTest;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BookServiceTest extends ServiceContextTest {
+
+    @Test
+    public void findByTitle() {
+        String expectedTitle = "We were liars";
+        List<Book> books = bookService.findByTitle(expectedTitle);
+
+        books.forEach(book -> {
+            String actualTitle = book.getTitle();
+            System.out.println(actualTitle);
+            Assert.assertTrue(actualTitle.contains(expectedTitle));
+        });
+    }
 }
